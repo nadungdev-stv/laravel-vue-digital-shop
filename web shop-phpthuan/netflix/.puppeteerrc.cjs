2032ed1cbc118f09098adc3532243dbbac5fc528
Na Dung <nadungdev@gmail.com>
2026-02-11 00:57:35 +0700

    Remove legacy PHP shop code

--- web shop-phpthuan/netflix/.puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-    // Config lưu Chrome ngay tại thư mục hiện tại thay vì thư mục user
-    cacheDirectory: path.join(__dirname, '.cache'),
-};
